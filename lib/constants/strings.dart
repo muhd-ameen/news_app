331f6a42da4a07900d8cd0ebba160af528281a39
muhd ameen <ameens.in19@gamil.com>
2021-08-22 14:44:41 +0530

    Initial commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,5 @@
+class Strings {
+  // ignore: non_constant_identifier_names
+  static String news_url =
+      'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=3bf7fa2dc03d46dcae2322fd025df39e';
+}
